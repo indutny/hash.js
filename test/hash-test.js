2e4816ddf1f7d21d32c667c1a62c72eb8e6861b4
Volker Mische <volker.mische@gmail.com>
2018-01-05 13:20:09 +0100

    hash: only pad when needed
    Only add padding if there are bytes pending. If the padding is
    added unconditionally then the hash changes on every `digest()`
    call.

--- test/hash-test.js
+++ test/hash-test.js
@@ -121,4 +121,17 @@ describe('Hash', function() {
       ]
     ]);
   });
+
+  it('should return the same digest in subsequent calls', function() {
+    var hashed = hash.sha256().update('abc');
+    assert.equal(
+      hashed.digest('hex'),
+      'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad');
+    assert.equal(
+      hashed.digest('hex'),
+      'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad');
+    assert.equal(
+      hashed.digest('hex'),
+      'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad');
+  });
 });
